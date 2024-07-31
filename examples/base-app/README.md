54b4d2c272ffdedfd070313c7a4164bc7651d6ce
ShineShao <xiaoshaoqq@gmail.com>
2024-08-01 01:41:10 +0800

    feat: hls examples

---
+++ examples/base-app/README.md
@@ -0,0 +1,16 @@
+## 使用
+
+
+
+```bash
+
+# 安装 http-server
+yarn install
+
+## 安装 `ezuikit-flv`, 拷贝 `node_modules/ezuikit-flv`下的 `decoder.js`, `decoder.wasm` 和 `index.js` 文件到当前目录下
+
+## 运行
+## http://localhost:8080
+yarn run dev
+
+```
---
+++ examples/vue-app/src/components/Player.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onMounted } from 'vue'
+import HLSPlayer from '@ezuikit/player-hls';
+
+let player = null
+
+onMounted(() => {
+  player = new HLSPlayer({
+    container: 'container',
+    url: 'https://open.ys7.com/v3/openlive/E71992743_1_1.m3u8',
+    decoder: 'decoder.js' // 软解解码资源 （wasm 要和js 在同一个文件夹中）
+  })
+  player.play()
+})
+</script>
+
+<template>
+  <div>
+    <div id="container"></div>
+  </div>
+</template>
+
+<style>
+#container {
+  width: 600px;
+  height: 400px;
+  background: #000;
+}
+</style>
