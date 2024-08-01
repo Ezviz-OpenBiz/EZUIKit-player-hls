84367b4565adacda6026eca5e27d94083e069360
ShineShao <xiaoshaoqq@gmail.com>
2024-08-01 12:50:12 +0800

    chore: update jsdoc

--- examples/vue-app/src/components/Player.vue
+++ examples/vue-app/src/components/Player.vue
@@ -5,6 +5,7 @@ import HlsPlayer from '@ezuikit/player-hls';
 let player = null
 
 onMounted(() => {
+  /** @type {HlsPlayer} */
   player = new HlsPlayer({
     id: 'container',
     url: 'https://open.ys7.com/v3/openlive/AA2615287_1_1.m3u8?expire=1753581976&id=739061430059499520&t=7842a33355d630ec646075c8637e59ae589e733f6b56105812ef37c2b78d0770&ev=100',
