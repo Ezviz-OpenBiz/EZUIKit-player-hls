f3be726a280f5e890334ec366179290ca56ca822
ShineShao <xiaoshaoqq@gmail.com>
2025-12-02 10:40:59 +0800

    demo: update hls v0.1.3

--- examples/vue-app/src/components/Player.vue
+++ examples/vue-app/src/components/Player.vue
@@ -9,7 +9,7 @@ onMounted(() => {
   /** @type {HlsPlayer} */
   player = new HlsPlayer({
     id: 'container',
-    url: 'https://open.ys7.com/v3/openlive/L28647609_1_1.m3u8?expire=1764621652&id=915734222503677952&t=f61fd1b82f26ab47b8e3e6a062a4c5ea80303850f21910f1cedeef908c782b68&ev=101&supportH265=1',
+    url: 'https://open.ys7.com/v3/openlive/L28647609_1_2.m3u8?expire=1764644823&id=915914456004136960&t=cbad467c0cc2dcafa9e8b663e1fcee506dc0ad9863e931d77b4423a25a6e3130&ev=101&supportH265=1',
   })
   // 默认自动播放
   // player.play()
