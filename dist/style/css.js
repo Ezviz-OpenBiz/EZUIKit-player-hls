b42cf2afd6e983aec46232a26975204168d574e6
ShineShao <xiaoshaoqq@gmail.com>
2026-07-17 19:01:18 +0800

    feat: remvoe console

--- dist/style/css.js
+++ dist/style/css.js
@@ -1,8 +1,7 @@
 /*
-*
-* DecoderWorker.js v2.0.0-beta.4
-* Copyright (c) 2026-7-16 Ezviz-OpenBiz
-*
-*/
+ * DecoderWorker.js v2.0.0-beta.5
+ * Copyright (c) 2026-7-17 Ezviz-OpenBiz
+ *
+ */
 "use strict";
 require("./css.css");
